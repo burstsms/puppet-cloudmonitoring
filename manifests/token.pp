a21e64b2ee02da2b67f4f55e03b6f062b0809f84
Mike Bissett <paran01d@gmail.com>
2014-04-30 12:53:45 +1000

    Use hostname instead of fqdn

--- manifests/token.pp
+++ manifests/token.pp
@@ -3,17 +3,17 @@ class cloudmonitoring::token {
 
   # create a token if it doesn't already exist
   exec { "raxmon-agent-tokens-create":
-    command     => "raxmon-agent-tokens-create --label=${::fqdn}",
+    command     => "raxmon-agent-tokens-create --label=${::hostname}",
     path        => ["/usr/local/bin", "/usr/bin/", "/bin"],
     environment => "RAXMON_RAXRC=${cloudmonitoring::raxmon::raxrc}",
     require     => Package["rackspace-monitoring-cli"],
-    unless      => "raxmon-agent-tokens-list | grep -i ${::fqdn}",
+    unless      => "raxmon-agent-tokens-list | grep -i ${::hostname}",
   }
 
   # now that the token exists, write the token to a file
-  $agent_token_filename = "/tmp/${::fqdn}.agent_token"
-  exec { "raxmon-agent-tokens-list ${::fqdn}":
-    command     => "raxmon-agent-tokens-list | grep -i ${::fqdn} | sed -n 's/.*id=\\([0-9a-f]*\\.[0-9]*\\).*/\\1/p' > ${agent_token_filename}",
+  $agent_token_filename = "/tmp/${::hostname}.agent_token"
+  exec { "raxmon-agent-tokens-list ${::hostname}":
+    command     => "raxmon-agent-tokens-list | grep -i ${::hostname} | sed -n 's/.*id=\\([0-9a-f]*\\.[0-9]*\\).*/\\1/p' > ${agent_token_filename}",
     path        => ["/usr/local/bin", "/usr/bin/", "/bin"],
     environment => "RAXMON_RAXRC=${cloudmonitoring::raxmon::raxrc}",
     creates     => $agent_token_filename,
@@ -24,26 +24,26 @@ class cloudmonitoring::token {
   # /etc/rackspace-monitoring-agent.cfg file
   $cfg_filename = "/etc/rackspace-monitoring-agent.cfg"
   exec { "create ${cfg_filename}":
-    command => "echo -n 'monitoring_token ' > ${cfg_filename} && cat ${agent_token_filename} >> ${cfg_filename} && echo 'monitoring_id ${::fqdn}' >> ${cfg_filename}",
+    command => "echo -n 'monitoring_token ' > ${cfg_filename} && cat ${agent_token_filename} >> ${cfg_filename} && echo 'monitoring_id ${::hostname}' >> ${cfg_filename}",
     path    => ["/usr/bin/", "/bin"],
     creates => $cfg_filename,
-    require => Exec["raxmon-agent-tokens-list ${::fqdn}"],
+    require => Exec["raxmon-agent-tokens-list ${::hostname}"],
     notify  => Class['cloudmonitoring::service'],
   }
 
   # get the agent id for this provider
-  $agent_id_filename = "/tmp/${::fqdn}.agent_id"
+  $agent_id_filename = "/tmp/${::hostname}.agent_id"
   exec { "create ${agent_id_filename}":
-    command     => "raxmon-entities-list | grep -i ${::fqdn} | sed -n 's/.*id=\\([a-zA-Z0-9]*\\) .*/\\1/p' > ${agent_id_filename}",
+    command     => "raxmon-entities-list | grep -i ${::hostname} | sed -n 's/.*id=\\([a-zA-Z0-9]*\\) .*/\\1/p' > ${agent_id_filename}",
     path        => ["/usr/local/bin", "/usr/bin/", "/bin"],
     environment => "RAXMON_RAXRC=${cloudmonitoring::raxmon::raxrc}",
     creates     => $agent_id_filename,
-    require     => Exec["raxmon-agent-tokens-list ${::fqdn}"],
+    require     => Exec["raxmon-agent-tokens-list ${::hostname}"],
   }
   
   # now just need to update the entity on rackspace to properly
   # include this agent's hostname
-  exec { "raxmon-entities-update --id=$(cat ${agent_id_filename}) --agent-id=${::fqdn}":
+  exec { "raxmon-entities-update --id=$(cat ${agent_id_filename}) --agent-id=${::hostname}":
     path        => ["/usr/local/bin", "/usr/bin/", "/bin"],
     environment => "RAXMON_RAXRC=${cloudmonitoring::raxmon::raxrc}",
     require     => Exec["create ${agent_id_filename}"],
